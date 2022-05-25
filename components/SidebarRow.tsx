22a3cd108a27c12965e353b71e53c648c04c8873
ankushbansal16 <84392810+ankushbansal16@users.noreply.github.com>
2022-05-26 00:43:32 +0530

    Init commit

---
+++ components/SidebarRow.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+interface Props {
+    Icon: (props: React.SVGProps<SVGSVGElement>) => JSX.Element
+    title: string
+    onClick?: () => {}
+}
+
+function SidebarRow({Icon, title, onClick}: Props) {
+  return (
+    <div onClick={() => onClick?.()} className='flex max-w-fit items-center space-x-2 px-4 py-3 rounded-full hover:bg-gray-100 transition-all duration-200 group cursor-pointer'>
+        <Icon className='h-6 w-6' />
+        <p className='hidden group-hover:text-twitter md:inline-flex text-base font-light lg:text-xl'>{title}</p>
+    </div>
+  )
+}
+
+export default SidebarRow
